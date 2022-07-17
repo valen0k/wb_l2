1b27b03cbc7e88db8d7882449ee9eb9a10a94147
valen0k <k.maksim-maksim@yandex.ru>
2022-07-17 16:34:18 +0300

    add pattern 01, 02

---
+++ pattern/01_facade.go
@@ -0,0 +1,82 @@
+package pattern
+
+import (
+	"errors"
+	"log"
+)
+
+/*
+Реализовать паттерн «фасад».
+Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
+https://en.wikipedia.org/wiki/Facade_pattern
+*/
+
+type WalletFacade struct {
+	wallet   *Wallet
+	Security *Security
+}
+
+func NewWalletFacade() *WalletFacade {
+	return &WalletFacade{
+		wallet:   NewWallet(),
+		Security: NewSecurity(),
+	}
+}
+
+func (f *WalletFacade) Buy(amount float32, code uint) error {
+	if f.Security.Check(code) {
+		err := f.wallet.DebitBalance(amount)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	return errors.New("некорректный код")
+}
+
+type Security struct {
+	code uint
+}
+
+func NewSecurity() *Security {
+	return &Security{code: 123}
+}
+
+func (s *Security) Check(code uint) bool {
+	return s.code == code
+}
+
+type Wallet struct {
+	balance float32
+}
+
+func NewWallet() *Wallet {
+	return &Wallet{balance: 0}
+}
+
+func (w *Wallet) CreditBalance(amount float32) {
+	w.balance += amount
+	log.Println("баланс пополнен")
+}
+
+func (w *Wallet) DebitBalance(amount float32) error {
+	if w.balance < amount {
+		return errors.New("на балансе недостаточно средств")
+	}
+	w.balance -= amount
+	log.Println("снятие с баланса пополнено")
+	return nil
+}
+
+/*
+Фасад — это структурный паттерн проектирования,
+который предоставляет простой интерфейс к сложной системе классов,
+библиотеке или фреймворку.
+
+
+Преимущества
+- Изолирует клиентов от компонентов сложной подсистемы.
+
+Недостатки
+- Фасад рискует стать божественным объектом, привязанным ко всем классам программы.
+*/
---
+++ pattern/02_builder.go
@@ -0,0 +1,92 @@
+package pattern
+
+/*
+Реализовать паттерн «строитель».
+Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
+https://en.wikipedia.org/wiki/Builder_pattern
+*/
+
+type IBuilder interface {
+	SetFloors(floors uint8)
+	SetWalls(walls uint8)
+	SetWindows(windows uint8)
+	SetDoors(doors uint8)
+	SetSquare(square float32)
+	SetRooms(rooms uint8)
+	Build() House
+}
+
+type House struct {
+	floors  uint8
+	walls   uint8
+	windows uint8
+	doors   uint8
+	square  float32
+	rooms   uint8
+}
+
+type MyHouse struct {
+	floors  uint8
+	walls   uint8
+	windows uint8
+	doors   uint8
+	square  float32
+	rooms   uint8
+}
+
+func NewMyHouse() *House {
+	return &House{}
+}
+
+func (h *MyHouse) SetFloors(floors uint8) {
+	h.floors = floors
+}
+
+func (h *MyHouse) SetWalls(walls uint8) {
+	h.walls = walls
+}
+
+func (h *MyHouse) SetWindows(windows uint8) {
+	h.windows = windows
+}
+
+func (h *MyHouse) SetDoors(doors uint8) {
+	h.doors = doors
+}
+
+func (h *MyHouse) SetSquare(square float32) {
+	h.square = square
+}
+
+func (h *MyHouse) SetRooms(rooms uint8) {
+	h.rooms = rooms
+}
+
+func (h *MyHouse) Build() House {
+	return House{
+		floors:  h.floors,
+		walls:   h.walls,
+		windows: h.windows,
+		doors:   h.doors,
+		square:  h.square,
+		rooms:   h.rooms,
+	}
+}
+
+/*
+Строитель — это порождающий паттерн проектирования,
+который позволяет создавать сложные объекты пошагово.
+Строитель даёт возможность использовать один и тот же код строительства
+для получения разных представлений объектов.
+
+
+Преимущества
+- Позволяет создавать продукты пошагово.
+- Позволяет использовать один и тот же код для создания различных продуктов.
+- Изолирует сложный код сборки продукта от его основной бизнес-логики.
+
+Недостатки
+- Усложняет код программы из-за введения дополнительных классов.
+- Клиент будет привязан к конкретным классам строителей,
+так как в интерфейсе директора может не быть метода получения результата.
+*/
